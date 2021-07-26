3dc0163699e26d5f3d8907e96608fb92aea97ad1
Nicholas Piggin <npiggin@gmail.com>
2021-07-26 13:50:18 +1000

    KVM: PPC: Book3S HV P9: Switch PMU to guest as late as possible
    This moves PMU switch to guest as late as possible in entry, and switch
    back to host as early as possible at exit. This helps the host get the
    most perf coverage of KVM entry/exit code as possible.
    
    This is slightly suboptimal for SPR scheduling point of view when the
    PMU is enabled, but when perf is disabled there is no real difference.
    
    Signed-off-by: Nicholas Piggin <npiggin@gmail.com>

--- arch/powerpc/kvm/book3s_hv_p9_entry.c
+++ arch/powerpc/kvm/book3s_hv_p9_entry.c
@@ -589,8 +589,6 @@ int kvmhv_vcpu_entry_p9(struct kvm_vcpu *vcpu, u64 time_limit, unsigned long lpc
 	local_paca->kvm_hstate.host_purr = mfspr(SPRN_PURR);
 	local_paca->kvm_hstate.host_spurr = mfspr(SPRN_SPURR);
 
-	switch_pmu_to_guest(vcpu, &host_os_sprs);
-
 	save_p9_host_os_sprs(&host_os_sprs);
 
 	/*
@@ -732,7 +730,9 @@ tm_return_to_guest:
 
 	accumulate_time(vcpu, &vcpu->arch.guest_time);
 
+	switch_pmu_to_guest(vcpu, &host_os_sprs);
 	kvmppc_p9_enter_guest(vcpu);
+	switch_pmu_to_host(vcpu, &host_os_sprs);
 
 	accumulate_time(vcpu, &vcpu->arch.rm_intr);
 
@@ -943,8 +943,6 @@ tm_return_to_guest:
 		asm volatile(PPC_CP_ABORT);
 
 out:
-	switch_pmu_to_host(vcpu, &host_os_sprs);
-
 	end_timing(vcpu);
 
 	return trap;
