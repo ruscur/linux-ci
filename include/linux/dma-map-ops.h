6c411ef634062b8ced132cf8bf8a586f21d7e392
Fox Snowpatch <fox@openpower.xyz>
2021-08-01 05:52:11 +0000

    From patchwork series 253696

--- include/linux/dma-map-ops.h
+++ include/linux/dma-map-ops.h
@@ -41,8 +41,10 @@ struct dma_map_ops {
 			size_t size, enum dma_data_direction dir,
 			unsigned long attrs);
 	/*
-	 * map_sg returns 0 on error and a value > 0 on success.
-	 * It should never return a value < 0.
+	 * map_sg should return a negative error code on error.
+	 * dma_map_sgtable() will return the error code returned by the
+	 * operation and dma_map_sg() will always convert any error to zero
+	 * to satisfy its own calling convention.
 	 */
 	int (*map_sg)(struct device *dev, struct scatterlist *sg, int nents,
 			enum dma_data_direction dir, unsigned long attrs);
