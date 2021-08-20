c8a4382c9a8a7c3576b03c2f5334348c8214f6d3
Fox Snowpatch <fox@openpower.xyz>
2021-08-20 01:49:04 +0000

    From patchwork series 258571
    https://patchwork.ozlabs.org//project/linuxppc-dev/list/?series=258571

--- arch/powerpc/perf/core-book3s.c
+++ arch/powerpc/perf/core-book3s.c
@@ -2254,15 +2254,8 @@ unsigned long perf_instruction_pointer(struct pt_regs *regs)
 	bool use_siar = regs_use_siar(regs);
 	unsigned long siar = mfspr(SPRN_SIAR);
 
-	if (ppmu && (ppmu->flags & PPMU_P10_DD1)) {
-		if (siar)
-			return siar;
-		else
-			return regs->nip;
-	} else if (use_siar && siar_valid(regs))
-		return mfspr(SPRN_SIAR) + perf_ip_adjust(regs);
-	else if (use_siar)
-		return 0;		// no valid instruction pointer
+	if (use_siar && siar_valid(regs) && siar)
+		return siar + perf_ip_adjust(regs);
 	else
 		return regs->nip;
 }
