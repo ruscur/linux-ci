42e3ed7637fe792eaa550fa21a19e75b90a7f471
Fox Snowpatch <fox@fox.ozlabs.org>
2021-12-03 13:21:27 +0000

    From patchwork series 275057
    https://patchwork.ozlabs.org//project/linuxppc-dev/list/?series=275057

--- drivers/pci/hotplug/rpaphp_pci.c
+++ drivers/pci/hotplug/rpaphp_pci.c
@@ -18,15 +18,65 @@
 #include "../pci.h"		/* for pci_add_new_bus */
 #include "rpaphp.h"
 
+/*
+ * RTAS call get-sensor-state(DR_ENTITY_SENSE) return values as per PAPR:
+ *    -1: Hardware Error
+ *    -2: RTAS_BUSY
+ *    -3: Invalid sensor. RTAS Parameter Error.
+ * -9000: Need DR entity to be powered up and unisolated before RTAS call
+ * -9001: Need DR entity to be powered up, but not unisolated, before RTAS call
+ * -9002: DR entity unusable
+ *  990x: Extended delay - where x is a number in the range of 0-5
+ */
+#define RTAS_HARDWARE_ERROR	-1
+#define RTAS_INVALID_SENSOR	-3
+#define SLOT_UNISOLATED		-9000
+#define SLOT_NOT_UNISOLATED	-9001
+#define SLOT_NOT_USABLE		-9002
+
+static int rtas_to_errno(int rtas_rc)
+{
+	int rc;
+
+	switch (rtas_rc) {
+	case RTAS_HARDWARE_ERROR:
+		rc = -EIO;
+		break;
+	case RTAS_INVALID_SENSOR:
+		rc = -EINVAL;
+		break;
+	case SLOT_UNISOLATED:
+	case SLOT_NOT_UNISOLATED:
+		rc = -EFAULT;
+		break;
+	case SLOT_NOT_USABLE:
+		rc = -ENODEV;
+		break;
+	case RTAS_BUSY:
+	case RTAS_EXTENDED_DELAY_MIN...RTAS_EXTENDED_DELAY_MAX:
+		rc = -EBUSY;
+		break;
+	default:
+		err("%s: unexpected RTAS error %d\n", __func__, rtas_rc);
+		rc = -ERANGE;
+		break;
+	}
+	return rc;
+}
+
 int rpaphp_get_sensor_state(struct slot *slot, int *state)
 {
+	int token = rtas_token("get-sensor-state");
 	int rc;
 	int setlevel;
 
-	rc = rtas_get_sensor(DR_ENTITY_SENSE, slot->index, state);
+	if (token == RTAS_UNKNOWN_SERVICE)
+		return -ENOENT;
+
+	rc = rtas_call(token, 2, 2, state, DR_ENTITY_SENSE, slot->index);
 
 	if (rc < 0) {
-		if (rc == -EFAULT || rc == -EEXIST) {
+		if (rc == SLOT_UNISOLATED  || rc == SLOT_NOT_UNISOLATED) {
 			dbg("%s: slot must be power up to get sensor-state\n",
 			    __func__);
 
@@ -38,15 +88,17 @@ int rpaphp_get_sensor_state(struct slot *slot, int *state)
 			if (rc < 0) {
 				dbg("%s: power on slot[%s] failed rc=%d.\n",
 				    __func__, slot->name, rc);
-			} else {
-				rc = rtas_get_sensor(DR_ENTITY_SENSE,
-						     slot->index, state);
+				return rc;
 			}
-		} else if (rc == -ENODEV)
+			rc = rtas_call(token, 2, 2, state, DR_ENTITY_SENSE,
+				       slot->index);
+		} else if (rc == SLOT_NOT_USABLE)
 			info("%s: slot is unusable\n", __func__);
 		else
 			err("%s failed to get sensor state\n", __func__);
 	}
+	if (rc)
+		rc = rtas_to_errno(rc);
 	return rc;
 }
 
