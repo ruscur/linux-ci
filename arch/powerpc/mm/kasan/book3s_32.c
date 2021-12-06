f7d35dac856be7deecd2384e7053e64d147f09d3
Fox Snowpatch <fox@fox.ozlabs.org>
2021-12-06 08:39:45 +0000

    From patchwork series 275305
    https://patchwork.ozlabs.org//project/linuxppc-dev/list/?series=275305

--- arch/powerpc/mm/kasan/book3s_32.c
+++ arch/powerpc/mm/kasan/book3s_32.c
@@ -11,32 +11,33 @@ int __init kasan_init_region(void *start, size_t size)
 	unsigned long k_start = (unsigned long)kasan_mem_to_shadow(start);
 	unsigned long k_end = (unsigned long)kasan_mem_to_shadow(start + size);
 	unsigned long k_cur = k_start;
-	int k_size = k_end - k_start;
-	int k_size_base = 1 << (ffs(k_size) - 1);
 	int ret;
 	void *block;
 
-	block = memblock_alloc(k_size, k_size_base);
+	while (k_cur < k_end) {
+		unsigned int k_size = bat_block_size(k_cur, k_end);
+		phys_addr_t phys;
+		int idx = find_free_bat();
 
-	if (block && k_size_base >= SZ_128K && k_start == ALIGN(k_start, k_size_base)) {
-		int k_size_more = 1 << (ffs(k_size - k_size_base) - 1);
+		if (idx == -1)
+			break;
+		if (k_size < SZ_128K)
+			break;
+		phys = memblock_phys_alloc(k_size, k_size);
+		if (!phys)
+			break;
 
-		setbat(-1, k_start, __pa(block), k_size_base, PAGE_KERNEL);
-		if (k_size_more >= SZ_128K)
-			setbat(-1, k_start + k_size_base, __pa(block) + k_size_base,
-			       k_size_more, PAGE_KERNEL);
-		if (v_block_mapped(k_start))
-			k_cur = k_start + k_size_base;
-		if (v_block_mapped(k_start + k_size_base))
-			k_cur = k_start + k_size_base + k_size_more;
-
-		update_bats();
+		setbat(idx, k_cur, phys, k_size, PAGE_KERNEL);
+		k_cur += k_size;
 	}
+	if (k_cur != k_start)
+		update_bats();
 
-	if (!block)
-		block = memblock_alloc(k_size, PAGE_SIZE);
-	if (!block)
-		return -ENOMEM;
+	if (k_cur < k_end) {
+		block = memblock_alloc(k_end - k_cur, PAGE_SIZE);
+		if (!block)
+			return -ENOMEM;
+	}
 
 	ret = kasan_init_shadow_page_tables(k_start, k_end);
 	if (ret)
