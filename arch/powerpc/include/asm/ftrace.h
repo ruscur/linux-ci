20a03df0b1f40dc1e9f179c6aac99d59d8554761
Fox Snowpatch <fox@fox.ozlabs.org>
2022-03-23 17:28:28 +0000

    From patchwork series 291708
    https://patchwork.ozlabs.org//project/linuxppc-dev/list/?series=291708

--- arch/powerpc/include/asm/ftrace.h
+++ arch/powerpc/include/asm/ftrace.h
@@ -107,11 +107,13 @@ static inline u8 this_cpu_get_ftrace_enabled(void)
 	return get_paca()->ftrace_enabled;
 }
 
+void ftrace_free_init_tramp(void);
 #else /* CONFIG_PPC64 */
 static inline void this_cpu_disable_ftrace(void) { }
 static inline void this_cpu_enable_ftrace(void) { }
 static inline void this_cpu_set_ftrace_enabled(u8 ftrace_enabled) { }
 static inline u8 this_cpu_get_ftrace_enabled(void) { return 1; }
+static inline void ftrace_free_init_tramp(void) { }
 #endif /* CONFIG_PPC64 */
 #endif /* !__ASSEMBLY__ */
 
