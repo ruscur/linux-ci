c5122ad628909f358d417aa4c3bc4194abd1bf44
Fox Snowpatch <fox@fox.ozlabs.org>
2022-08-15 20:04:39 +0000

    From patchwork series 313906
    https://patchwork.ozlabs.org//project/linuxppc-dev/list/?series=313906

--- arch/powerpc/boot/epapr.c
+++ arch/powerpc/boot/epapr.c
@@ -21,7 +21,7 @@ BSS_STACK(4096);
 #define EPAPR_SMAGIC	0x65504150
 #define EPAPR_EMAGIC	0x45504150
 
-static unsigned epapr_magic;
+static unsigned int epapr_magic;
 static unsigned long ima_size;
 static unsigned long fdt_addr;
 
