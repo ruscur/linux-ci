c0019a227f0ae605179650c8a5fdd5e134b1c38c
Fox Snowpatch <fox@fox.ozlabs.org>
2022-11-25 03:07:54 +0000

    From patchwork series 329083
    https://patchwork.ozlabs.org//project/linuxppc-dev/list/?series=329083

--- arch/powerpc/kernel/udbg.c
+++ arch/powerpc/kernel/udbg.c
@@ -69,6 +69,9 @@ void __init udbg_early_init(void)
 	udbg_init_debug_opal_hvsi();
 #elif defined(CONFIG_PPC_EARLY_DEBUG_16550)
 	udbg_init_debug_16550();
+#elif defined(CONFIG_PPC_EARLY_DEBUG_LATTEIPC)
+	/* For use on Nintendo Wii U */
+	udbg_init_latteipc();
 #endif
 
 #ifdef CONFIG_PPC_EARLY_DEBUG
