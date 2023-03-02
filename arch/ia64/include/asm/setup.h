bfbacf2f858220d24bee91f1fead72abd15ba87a
Fox Snowpatch <fox@fox.ozlabs.org>
2023-03-02 10:10:29 +0000

    From patchwork series 344379
    https://patchwork.ozlabs.org//project/linuxppc-dev/list/?series=344379

---
+++ arch/ia64/include/asm/setup.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+
+#ifndef __IA64_SETUP_H
+#define __IA64_SETUP_H
+
+#include <uapi/asm/setup.h>
+
+#define COMMAND_LINE_SIZE	2048
+
+#endif
