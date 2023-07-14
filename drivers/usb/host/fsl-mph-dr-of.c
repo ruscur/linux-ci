4dfd810903d9e06f228992025b4141f62eddd8b6
Fox Snowpatch <fox@fox.ozlabs.org>
2023-07-14 17:55:22 +0000

    From patchwork series 363958
    https://patchwork.ozlabs.org//project/linuxppc-dev/list/?series=363958

--- drivers/usb/host/fsl-mph-dr-of.c
+++ drivers/usb/host/fsl-mph-dr-of.c
@@ -10,10 +10,12 @@
 #include <linux/fsl_devices.h>
 #include <linux/err.h>
 #include <linux/io.h>
-#include <linux/of_platform.h>
+#include <linux/of.h>
+#include <linux/of_device.h>
 #include <linux/clk.h>
 #include <linux/module.h>
 #include <linux/dma-mapping.h>
+#include <linux/of.h>
 
 struct fsl_usb2_dev_data {
 	char *dr_mode;		/* controller mode */
